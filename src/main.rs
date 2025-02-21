95888e300a4eca7df5664c182f9ff1743f98b269
romann <romann.belmer@gmail.com>
2025-02-21 09:53:27 +0100

    added running loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut line = String::new();
+    let mut input: String = String::new();
+    loop {
+        std::io::stdin().read_line(&mut line).expect("Erreur de lecture");
+        let trimmed = line.trim();
+        if trimmed.is_empty() {
+            // add code
+        } else {
+            input.push_str("\n");
+            input.push_str(trimmed);
+        }
+        input = String::new();
+    }
 }
