55ec9eda23661b8903e008089bc88cd130040d70
romann <romann.belmer@gmail.com>
2025-02-21 10:13:58 +0100

    new file

---
+++ src/parsing/mod.rs
@@ -0,0 +1,10 @@
+//! Parse all input for the filler project
+#![allow(unused_imports)]
+
+mod piece;
+mod player;
+mod table;
+
+use piece::*;
+use player::*;
+use table::*;
---
+++ src/parsing/player.rs
@@ -0,0 +1,34 @@
+#![allow(dead_code)]
+
+const DEFAULT_PLAYER1: &str = "@a";
+const DEFAULT_PLAYER2: &str = "$s";
+
+pub struct Player<'a> {
+	id: usize,
+	chars: &'a str,
+}
+
+impl<'a> Player<'a> {
+    pub fn new(id: usize, chars: Option<&'a str>) -> Self {
+        let chars = match chars {
+            Some(chars) => chars,
+            None => match id {
+                1 => &DEFAULT_PLAYER1,
+                2 => &DEFAULT_PLAYER2,
+                _ => panic!("no default chars set for this player"),  
+            },
+        };
+        Self {
+            id,
+            chars,
+        }
+    }
+
+    pub fn id(&self) -> usize {
+        self.id
+    }
+
+    pub fn chars(&self) -> &str {
+        &self.chars
+    }
+}
